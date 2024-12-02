668d8770c7c3d4dc7eefbc7f70c7140869254357
Pablo <pablo@digito.pe>
2024-12-02 18:15:39 -0500

    changes migrations

--- database/migrations/2024_11_13_174847_add_foreign_key_to_equipment_logs_table.php
+++ database/migrations/2024_11_13_174847_add_foreign_key_to_equipment_logs_table.php
@@ -9,20 +9,14 @@ return new class extends Migration
     public function up()
     {
         Schema::table('equipment_logs', function (Blueprint $table) {
-            // Agregamos el índice y la llave foránea para equipment_id
+            // Solo agregamos el índice
             $table->index('equipment_id');
-            $table->foreign('equipment_id')
-                ->references('id')
-                ->on('equipments')
-                ->onDelete('cascade')
-                ->onUpdate('no action');
         });
     }
 
     public function down()
     {
         Schema::table('equipment_logs', function (Blueprint $table) {
-            $table->dropForeign(['equipment_id']);
             $table->dropIndex(['equipment_id']);
         });
     }
